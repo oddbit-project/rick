dbc05cba219e7d5f61d2cd0108da65745ed918e6
jpinheiro <jpinheiro@blackshield.pt>
2021-11-17 16:52:34 +0000

    reliability fix: trap of json exceptions

--- rick/resource/config/json.py
+++ rick/resource/config/json.py
@@ -1,4 +1,5 @@
 import json
+from json import JSONDecodeError
 
 from rick.base import ShallowContainer
 
@@ -7,7 +8,7 @@ def json_file(filename: str) -> ShallowContainer:
     try:
         with open(filename) as cfg_file:
             contents = json.loads(cfg_file.read())
-    except IOError as e:
+    except (IOError, JSONDecodeError) as e:
         raise RuntimeError("an exception occurred when loading config file {}: {}".format(filename, str(e)))
 
     return ShallowContainer(contents)
