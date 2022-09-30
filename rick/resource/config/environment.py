b9a7460e9369bdc8980679528c8b239de2ffc448
jpinheiro <jpinheiro@blackshield.pt>
2022-09-30 18:05:05 +0100

    Container-based Environment Config

--- rick/resource/config/environment.py
+++ rick/resource/config/environment.py
@@ -1,21 +1,104 @@
+import json
 import os
-
+from rick.base import ShallowContainer
+from typing import Any, List
 
 class EnvironmentConfig:
     """
-    Base class for attribute-based config
+    Base class for environment-based config
 
-    Config attributes must be defined as uppercase; on init, they are replaced with existing env values, if set
-    Note: All replaceable values must be of string type
+    Config attributes must be named uppercase; These attribute names will be translated to lowercase
+    on the ShallowContainer; The uppercase names are used to override values from existing environment variables
+
+    Example:
+        EXISTING OS ENV VARS: [DB_NAME="some_db", DB_HOST="abc"]
+
+        class MyConfig(EnvironmentConfig):
+            DB_NAME = 'mydb'
+            DB_HOST = 'localhost'
+            DB_USERNAME = 'username'
+            DB_PASSWORD = 'password'
+
+        cfg = MyConfig().build()
+        assert cfg['db_name'] == 'some_db'
+        assert cfg['db_host'] == 'abc'
+        assert cfg['db_username'] == 'abc'
     """
+    list_separator = ','
 
-    def __init__(self, prefix=''):
+    def build(self, prefix='') -> ShallowContainer:
         """
-        Load override values from OS environment variables
-        :param prefix: optional variable prefix
+        Assemble a final ShallowContainer based on the env vars
+        Note: The env vars also override the object values
+        :param prefix: optional prefix name for env vars
+        :return: ShallowContainer
         """
+        data = {}
         for name in dir(self):
             if name.isupper():
                 value = getattr(self, name)
                 if not callable(value):
-                    setattr(self, name, os.environ.get(prefix+name, value))
+                    value = self._parse_value(prefix + name, value)
+                    setattr(self, name, value)
+                    data[name.lower()] = value
+        return ShallowContainer(data)
+
+    def _parse_value(self, env_var_name, existing_value) -> Any:
+        """
+        Simple mapper to extract environment variables based on type
+
+        :param env_var_name: env var to process
+        :param existing_value: existing default value
+        :return: overridden value of correct existing_value type, if env var exists; existing_value otherwise
+        """
+        value = os.environ.get(env_var_name)
+        if value is None:
+            return existing_value
+
+        # if default value of attribute is none, always assume string
+        if existing_value is None:
+            return value
+
+        mapper = getattr(self, "_{}_conv".format(type(existing_value).__name__))
+        if not mapper:
+            raise ValueError("Invalid data type detected when parsing environment variable '{}'".format(env_var_name))
+        return mapper(value)
+
+    def _str_conv(self, v) -> str:
+        """
+        String mapper
+        :param v:
+        :return: str
+        """
+        return str(v)
+
+    def _int_conv(self, v) -> int:
+        """
+        Int mapper
+        :param v:
+        :return: int
+        """
+        return int(v)
+
+    def _list_conv(self, v) -> List:
+        """
+        List mapper
+        :param v: a string containing multiple string values, separated by self.list_separator
+        :return: List
+        """
+        if not type(v) is str:
+            raise ValueError("Invalid data type to extract list: expecting str, got '{}'".format(type(v).__name__))
+        return str(v).split(self.list_separator)
+
+    def _dict_conv(self, v) -> dict:
+        """
+        Dict mapper
+        :param v: a json string containing an object (dict)
+        :return: dict
+        """
+        if not type(v) is str:
+            raise ValueError("Invalid data type to extract dict: expecting str, got '{}'".format(type(v).__name__))
+        try:
+            return json.loads(v)
+        except e:
+            raise ValueError("Error when parsing JSON: {}".format(e))
--- tests/resource/test_config_environment.py
+++ tests/resource/test_config_environment.py
@@ -1,38 +1,86 @@
 import os
 import pytest
 from rick.resource.config import EnvironmentConfig
+from rick.base import ShallowContainer
 
 class ConfigTest1(EnvironmentConfig):
     OPTION_1 = None
     OPTION_2 = 'x'
 
+class ConfigTest2(EnvironmentConfig):
+    FOO_LIST = []
+    FOO_INT = 1
+    FOO_STR = None
+    FOO_DICT = {}
 
 fixture_configtest1 = [(ConfigTest1, {'OPTION_1': 'abc', 'OPTION_2': 'def'})]
+fixture_configtest_prefix = [(ConfigTest1, {'PREFIX_OPTION_1': 'abc', 'PREFIX_OPTION_2': 'def'})]
+
+fixture_configtest2 = [(
+    ConfigTest2,
+    { # env vars
+        'FOO_LIST': 'abc,def',
+        'FOO_INT': '5',
+        'FOO_STR': 'joe',
+        'FOO_DICT':'{"key":"value"}'
+    }, # expected result
+    {
+        'foo_list': ['abc', 'def'],
+        'foo_int': 5,
+        'foo_str': 'joe',
+        'foo_dict': {"key":"value"}
+    }
+)]
 
 @pytest.mark.parametrize("fixture", fixture_configtest1)
-def test_ConfigEnvironment(fixture):
+def test_EnvConfig(fixture):
     for name, value in fixture[1].items():
         os.environ[name] = str(value)
 
     # build config object
-    cfg = fixture[0]()
+    cfg = fixture[0]().build()
 
     # check if values were overridden
     for name, value in fixture[1].items():
-        assert getattr(cfg, name) == str(value)
-
-fixture_configtest_prefix = [(ConfigTest1, {'PREFIX_OPTION_1': 'abc', 'PREFIX_OPTION_2': 'def'})]
+        assert cfg.get(name.lower()) == str(value)
 
 @pytest.mark.parametrize("fixture", fixture_configtest_prefix)
-def test_ConfigEnvironment_prefix(fixture):
+def test_EnvConfig_prefix(fixture):
     for name, value in fixture[1].items():
         os.environ[name] = str(value)
 
     # build config object
     prefix = 'PREFIX_'
-    cfg = fixture[0](prefix)
+    cfg = fixture[0]().build(prefix)
 
     # check if values were overridden
     for name, value in fixture[1].items():
-        assert getattr(cfg, name.replace(prefix, '')) == str(value)
+        assert cfg.get(name.replace(prefix, '').lower()) == str(value)
 
+
+@pytest.mark.parametrize("fixture", fixture_configtest2)
+def test_EnvConfig_types(fixture):
+    obj = fixture[0]()
+
+    # first, check that build() processes correctly without any set env variables
+    cfg = obj.build()
+    for name in dir(obj):
+        if name.isupper():
+            value = cfg.get(name.lower())
+            if isinstance(value, ShallowContainer):
+                # unrap dict from ShallowContainer
+                value = value.asdict()
+            assert value == getattr(obj, name)
+
+    # now set env variables
+    for name, value in fixture[1].items():
+        os.environ[name] = str(value)
+
+    # re-build cfg with overriden values
+    cfg = obj.build()
+    # verify overriden values match expected values
+    for name, value in fixture[1].items():
+        value = cfg.get(name.lower())
+        if isinstance(value, ShallowContainer):
+            value = value.asdict()
+        assert value == fixture[2][name.lower()]
