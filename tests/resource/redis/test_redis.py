630d8a53afc6ae422ca5b8ea7a68410f62e6bd2a
jpinheiro <jpinheiro@blackshield.pt>
2025-07-30 09:25:52 +0100

    unit test updates; documentation updates

--- tests/resource/redis/test_redis.py
+++ tests/resource/redis/test_redis.py
@@ -1,31 +1,13 @@
 from time import sleep
 
-import pytest
-
 from rick.base import ShallowContainer
-from rick.resource.redis import RedisCache
-
-
-@pytest.fixture
-def redis_cfg():
-    return {
-        "host": "localhost",
-        "port": 63790,
-        "password": "myRedisPassword",
-        "db": 0,
-        "ssl": False,
-    }
-
-
-@pytest.fixture
-def redis_client(redis_cfg):
-    return RedisCache(**redis_cfg)
 
 
 class TestRedisCache:
-    def test_get_set_remove(self, redis_cfg, redis_client):
+    def test_get_set_remove(self, redis_client):
         key = "shallow:cfg"
-        obj = ShallowContainer(redis_cfg)
+        test_data = {"foo": "bar", "baz": 123}
+        obj = ShallowContainer(test_data)
         assert redis_client.has(key) is False
         redis_client.set(key, obj)
         assert redis_client.has(key) is True
@@ -33,25 +15,27 @@ class TestRedisCache:
         record = redis_client.get(key)
         assert record is not None
         assert isinstance(record, ShallowContainer)
-        assert list(record.asdict().keys()) == list(redis_cfg.keys())
+        assert list(record.asdict().keys()) == list(test_data.keys())
 
-        assert redis_client.remove(key) == 1
+        assert redis_client.remove(key) is True
         assert redis_client.has(key) is False
-        assert redis_client.remove(key) == 0
+        assert redis_client.remove(key) is False
         assert redis_client.has(key) is False
 
-    def test_ttl(self, redis_cfg, redis_client):
+    def test_ttl(self, redis_client):
         key = "shallow:cfg"
-        obj = ShallowContainer(redis_cfg)
+        test_data = {"foo": "bar", "ttl": "test"}
+        obj = ShallowContainer(test_data)
         assert redis_client.has(key) is False
         redis_client.set(key, obj, ttl=1)
         assert redis_client.has(key) is True
         sleep(2)
         assert redis_client.has(key) is False
 
-    def test_purge(self, redis_cfg, redis_client):
+    def test_purge(self, redis_client):
         key = "shallow:cfg"
-        obj = ShallowContainer(redis_cfg)
+        test_data = {"foo": "bar", "purge": "test"}
+        obj = ShallowContainer(test_data)
         assert redis_client.has(key) is False
         redis_client.set(key, obj)
         assert redis_client.has(key) is True
